9bba9ee44672530153e414671dba253ea8f722e4
lwcc20090709 <115065039+lwcc20090709@users.noreply.github.com>
2022-10-19 15:00:43 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
 let value_x = 0
 let value_Y = 0
-input.onButtonPressed(Button.A, function () {
-    value_x = 0
-    for (let index = 0; index < 5; index++) {
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
         led.plot(value_x, 0)
         basic.pause(200)
-        basic.clearScreen()
         value_x += 1
     }
+    if (value_x > 4) {
+        value_x = 0
+    }
 })
-input.onButtonPressed(Button.B, function () {
-    value_Y = 0
-    while (value_Y >= 0) {
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
         led.plot(0, value_Y)
         basic.pause(200)
-        basic.clearScreen()
         value_Y += 1
+    }
+    if (value_Y > 4) {
+        value_Y = 0
     }
 })
